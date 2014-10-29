040be64f8fae8dcffea30df275920c2ed499a168
martino <manda.mgf@gmail.com>
2014-10-29 15:36:43 +0100

    Resizable left panel

--- gui/EditorGUI.py
+++ gui/EditorGUI.py
@@ -1,6 +1,7 @@
 __author__ = 'martino'
 
-from PyQt4.QtGui import QWidget, QGridLayout
+from PyQt4.QtGui import QWidget, QGridLayout, QPainter, QPaintEvent, QColor, QPen, QMouseEvent
+from PyQt4.Qt import Qt, QPoint
 from gui.Containers import EditorContainer
 
 
@@ -14,8 +15,59 @@ class OIMainToolbar(QWidget):
 class OIRightShelf(QWidget):
     def __init__(self, parent):
         QWidget.__init__(self, parent)
-        self.setMaximumWidth(250)
-        self.setMinimumWidth(100)
+        self.setFixedWidth(250)
+        self.setMouseTracking(True)
+        self.__drag = False
+
+    def paintEvent(self, e: QPaintEvent):
+        if not e.isAccepted():
+            return
+        p = QPainter(self)
+        p.setRenderHint(QPainter.Antialiasing)
+        p.setPen(QColor(0, 0, 0, 0))
+        p.setBrush(QColor(55, 50, 47))
+        p.drawRect(0, 0, 11, self.height())
+
+        if self.__drag:
+            p.setBrush(QColor(255, 255, 255, 180))
+        else:
+            p.setBrush(QColor(0, 0, 0, 180))
+        p.setPen(QPen(QColor(255, 255, 255), 0.5))
+        p.drawRoundedRect(1, 10, 20, 20, 2, 2)
+        p.setBrush(QColor(205, 205, 205, 180))
+        p.drawEllipse(4, 18, 4, 4)
+
+        p.setBrush(self.palette().color(self.backgroundRole()))
+        p.setPen(QColor(0, 0, 0, 0))
+        p.drawRect(10, 0, self.width() - 10, self.height())
+        p.setPen(QColor(0, 0, 0))
+        p.drawLine(10, 0, 10, self.height())
+
+    def mousePressEvent(self, e: QMouseEvent):
+        if e.x() <= 12 and 9 <= e.y() <= 31 and e.button() == Qt.LeftButton:
+            self.__drag = True
+            self.repaint()
+        else:
+            self.__drag = False
+
+    def mouseReleaseEvent(self, e):
+        self.__drag = False
+        self.repaint()
+
+    def mouseMoveEvent(self, e: QMouseEvent):
+        if self.__drag:
+            w = self.width() - e.x()
+            if w <= 11:
+                w = 11
+            if w >= 450:
+                w = 450
+            self.setFixedWidth(w)
+        elif e.x() <= 12 and 9 <= e.y() <= 31:
+            self.setCursor(Qt.PointingHandCursor)
+        else:
+            self.setCursor(Qt.ArrowCursor)
+
+            # self.setMinimumWidth(e.x())
 
 
 class OIMainWindow(QWidget):
@@ -25,6 +77,7 @@ class OIMainWindow(QWidget):
         self.setMinimumSize(600, 400)
         self.__layout = QGridLayout(self)
         self.__layout.setMargin(0)
+        self.__layout.setSpacing(0)
         self.__editor = EditorContainer(self)
         self.__toolbar = OIMainToolbar(self)
         self.__right_shelf = OIRightShelf(self)
