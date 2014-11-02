f8ad9107e5e7c15c69a5d38a8757fd2129a78004
Mandarancio <manda.mgf@gmail.com>
2014-11-02 20:05:51 +0100

    Start working on main interface

---
+++ gui/OIWorkingSpace.py
@@ -0,0 +1,13 @@
+__author__ = 'martino'
+
+from PyQt4.QtGui import QWidget
+
+
+class OIWorkingSpace(QWidget):
+    def __int__(self, parent=None):
+        QWidget.__init__(self, parent)
+
+
+class OIWSWidget(QWidget):
+    def __init__(self, parent=None):
+        QWidget.__init__(self, parent)
